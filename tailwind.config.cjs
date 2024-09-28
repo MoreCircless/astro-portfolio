4bfad0febe0faf803f5045ad64bdc1c920aa9993
pablo <pablo@pablo.pablo>
2024-09-28 18:06:39 +0200

    added typewriter on splash

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,20 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      keyframes: {
+        typewriter: {
+          '0%': { width: '0%' },
+          '100%': { width: '100%' },
+        },
+        blink: {
+          '50%': { 'border-color': 'transparent' },
+          '100%': { 'border-color': 'white' },
+        },
+      },
+      animation: {
+        typewriter: 'typewriter 2s steps(30, end) 1s 1 normal both',
+        blink: 'blink 0.75s step-end infinite',
+      },
       fontFamily: {
         sans: ["Inter Variable", ...defaultTheme.fontFamily.sans],
       },
